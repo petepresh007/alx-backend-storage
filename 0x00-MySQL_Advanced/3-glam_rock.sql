f02c7dfbee33cd7410251cec7ac62aaef5d08040
Precious Peter <peterprecious370@gmail.com>
2024-06-13 12:17:16 -0700

    fixed 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,13 +1,5 @@
--- all band with glamrock 
-SELECT
-    band_name,
-    IFNULL(
-        2022 - formed,
-        2022 - formed + (split - formed)
-    ) AS lifespan
-FROM
-    metal_bands
-WHERE
-    style = 'Glam rock'
-ORDER BY
-    lifespan DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
