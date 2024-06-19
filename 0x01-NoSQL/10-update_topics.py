d13f00fd3f19852663888783a6fde0eb82f17836
peter precious <peteprecious370@gmail.com>
2024-06-19 11:00:38 -0700

    update

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''a module to update a document'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''a function to update'''
+    result = mongo_collection.update_one(
+            {"name": name}, {"$set": {"topics": topics}}
+            )
+    return result
